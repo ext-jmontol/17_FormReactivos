ab81682bf83ebed011a1672661bf0b9adb454127
Jose Montesdeoca <jjmontesdeoca@inerza.com>
2023-11-27 07:53:51 +0000

    Validaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
+import { SideMenuComponent } from './src/app/shared/components/side-menu/side-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideMenuComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
